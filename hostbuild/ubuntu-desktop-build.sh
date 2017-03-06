2e0126e3cef6a35ba453a5d772e403c9baefa1ca
andrew-best-diaxion <abest@diaxion.com>
2017-03-06 15:38:08 +1100

    Ubuntu desktop build script
    First cut at this script.

---
+++ hostbuild/ubuntu-desktop-build.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# configure our base Ubuntu image
+# based on https://confluence.diaxion.com/display/~abest/Ubuntu+Dev+VM+Build
+
+
+# NOTE - not required on a desktop machine.
+#echo "recreating SSH server host keys"
+## This regenerates our host keys
+#sudo sh -c "dpkg-reconfigure openssh-server"
+
+# add the Google Chrome repo so we can install Chrome a little later.
+# https://www.ubuntuupdates.org/ppa/google_chrome?dist=stable
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+
+# add the Atom repo
+# Atom unofficial PPA: http://tipsonubuntu.com/2016/08/05/install-atom-text-editor-ubuntu-16-04/
+sudo add-apt-repository ppa:webupd8team/atom
+
+# Update the package indexes
+echo "Updating apt repo package indexes"
+sudo apt-get update && sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confnew" dist-upgrade
+
+# Install other Ubuntu packages that are useful
+echo "installing some base tools and open-vm-tools-desktop"
+sudo apt-get -y -o DPkg::Options::="--force-confnew" install open-vm-tools-desktop htop sysstat dstat iotop python python-pip google-chrome-stable atom
+
+# Configure automatic security updates
+echo "configuring auto install of security updates"
+sudo bash -c "cat > /etc/apt/apt.conf.d/20auto-upgrades" << "EOF"
+APT::Periodic::Update-Package-Lists "1";
+APT::Periodic::Unattended-Upgrade "1";
+EOF
+sudo /etc/init.d/unattended-upgrades restart
+
+# Upgrade python-pip
+sudo pip install --upgrade pip
+
+# install AWS cli
+sudp pip install awscli aws-shell
+
+# Disable guest access
+echo "disabling guest access"
+sudo bash -c "cat /etc/lightdm/lightdm.conf.d/50-no-guest.conf" << "EOF"
+[SeatDefaults]
+allow-guest=false
+EOF
+
+# now we clean up left over packages
+sudo apt-get auto-remove
+
+# and then restart
+sudo init 6
