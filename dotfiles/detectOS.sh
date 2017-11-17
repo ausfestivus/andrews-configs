f1e5c20560c50ec5325f6118a5cf72d6d7dc6fb5
andrew-best-diaxion <abest@diaxion.com>
2017-11-17 16:51:15 +1100

    adds support for MacOS High Sierra to other scripts
    Fixes #31

--- dotfiles/detectOS.sh
+++ dotfiles/detectOS.sh
@@ -4,12 +4,12 @@
 # This script detects which OS were running in.
 ############################
 
-if  [ $OSTYPE == "darwin16" ]
-  # macOS specific commands go here
-  then
+if  [ $OSTYPE == "darwin16" ] || [ $OSTYPE == "darwin17" ]
+# macOS specific commands go here
+then
   echo "Hi, im a Mac and my name is '$HOSTNAME'"
 elif [ $OSTYPE == "linux-gnu" ]
-  # Linux specifc commands go here
-  then
+# Linux specific commands go here
+then
   echo "Hi, im a Linux machine and my name is '$HOSTNAME'"
 fi
