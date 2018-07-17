fc8e4ceeaa3b8dc130a940cd7e93243b40a37a92
ausfestivus <abest@diaxion.com>
2018-07-17 12:06:42 +1000

    refactor home vnet and jumphost

--- terraform/azureControlPlaneHome/jumphost.tf
+++ terraform/azureControlPlaneHome/jumphost.tf
@@ -1,18 +1,19 @@
 # -----------------------------------------------------------------------------
-# "jumpbox" configuration
+# "jumpvm" configuration
 # -----------------------------------------------------------------------------
 
-resource "azurerm_public_ip" "jumpbox" {
-  name                         = "jumpbox-public-ip"
+resource "azurerm_public_ip" "jumpvm" {
+  name                         = "${var.jumpvm_hostname}-ip"
   location                     = "${var.location}"
   resource_group_name          = "${azurerm_resource_group.rg.name}"
   public_ip_address_allocation = "static"
-  domain_name_label            = "${azurerm_resource_group.rg.name}-ssh"
-  tags                         = "${var.tags}"
+
+  #domain_name_label            = "${azurerm_resource_group.rg.name}-ssh"
+  tags = "${var.tags}"
 }
 
-resource "azurerm_network_security_group" "jumpbox" {
-  name                = "jumpbox-ssh-access"
+resource "azurerm_network_security_group" "jumpvm" {
+  name                = "${var.jumpvm_hostname}-ssh-access"
   location            = "${var.location}"
   resource_group_name = "${azurerm_resource_group.rg.name}"
   tags                = "${var.tags}"
@@ -20,99 +21,84 @@ resource "azurerm_network_security_group" "jumpbox" {
 
 resource "azurerm_network_security_rule" "ssh_access" {
   name                        = "ssh-access-rule"
-  network_security_group_name = "${azurerm_network_security_group.jumpbox.name}"
+  network_security_group_name = "${azurerm_network_security_group.jumpvm.name}"
   direction                   = "Inbound"
   access                      = "Allow"
   priority                    = 200
   source_address_prefix       = "*"
   source_port_range           = "*"
-  destination_address_prefix  = "${azurerm_network_interface.jumpbox.private_ip_address}"
+  destination_address_prefix  = "${azurerm_network_interface.jumpvm.private_ip_address}"
   destination_port_range      = "22"
   protocol                    = "TCP"
   resource_group_name         = "${azurerm_resource_group.rg.name}"
 }
 
-resource "azurerm_network_security_rule" "www_access" {
+resource "azurerm_network_security_rule" "https_access" {
   name                        = "www-access-rule"
-  network_security_group_name = "${azurerm_network_security_group.jumpbox.name}"
+  network_security_group_name = "${azurerm_network_security_group.jumpvm.name}"
   direction                   = "Inbound"
   access                      = "Allow"
   priority                    = 201
   source_address_prefix       = "*"
   source_port_range           = "*"
-  destination_address_prefix  = "${azurerm_network_interface.jumpbox.private_ip_address}"
-  destination_port_range      = "80"
+  destination_address_prefix  = "${azurerm_network_interface.jumpvm.private_ip_address}"
+  destination_port_range      = "443"
   protocol                    = "TCP"
   resource_group_name         = "${azurerm_resource_group.rg.name}"
 }
 
-# resource "azurerm_network_security_rule" "admin_access" {
-#   name                        = "remote-admin-access-rule"
-#   network_security_group_name = "${azurerm_network_security_group.unitingAzureFoundation-nsg.name}"
-#   resource_group_name         = "${azurerm_resource_group.rg.name}"
-#   direction                   = "Inbound"
-#   access                      = "Allow"
-#   priority                    = 200
-#   #source_address_prefixes     = ["${var.unitingPublicIPrange}"]
-#   source_address_prefix       = "*"
-#   source_port_range           = "*"
-#   destination_address_prefixes  = ["${var.subnet_prefixes}"]
-#   destination_port_ranges      = ["22", "3389"]
-#   protocol                    = "TCP"
-# }
-
-resource "azurerm_network_interface" "jumpbox" {
-  name                      = "jumpbox-nic"
+resource "azurerm_network_interface" "jumpvm" {
+  name                      = "${var.jumpvm_hostname}-nic"
   location                  = "${var.location}"
   resource_group_name       = "${azurerm_resource_group.rg.name}"
-  network_security_group_id = "${azurerm_network_security_group.jumpbox.id}"
+  network_security_group_id = "${azurerm_network_security_group.jumpvm.id}"
 
   ip_configuration {
     name                          = "IPConfiguration"
     subnet_id                     = "${module.network.vnet_subnets[0]}"
-    private_ip_address_allocation = "dynamic"
-    public_ip_address_id          = "${azurerm_public_ip.jumpbox.id}"
+    private_ip_address_allocation = "static"
+    private_ip_address            = "${var.jumpvm_private_ip}"
+    public_ip_address_id          = "${azurerm_public_ip.jumpvm.id}"
   }
 
-  tags                      = "${var.tags}"
+  tags = "${var.tags}"
 }
 
-resource "azurerm_virtual_machine" "jumpbox" {
-  name                          = "jumpbox"
+resource "azurerm_virtual_machine" "jumpvm" {
+  name                          = "${var.jumpvm_hostname}-vm"
   location                      = "${var.location}"
   resource_group_name           = "${azurerm_resource_group.rg.name}"
-  network_interface_ids         = ["${azurerm_network_interface.jumpbox.id}"]
-  vm_size                       = "Standard_DS1_v2"
+  network_interface_ids         = ["${azurerm_network_interface.jumpvm.id}"]
+  vm_size                       = "${var.vm_size}"
   delete_os_disk_on_termination = true
 
   storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
+    publisher = "${var.image_publisher}"
+    offer     = "${var.image_offer}"
+    sku       = "${var.image_sku}"
+    version   = "${var.image_version}"
   }
 
   storage_os_disk {
-    name              = "jumpbox-osdisk"
+    name              = "jumpvm-osdisk"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
 
   os_profile {
-    computer_name  = "jumpbox"
-    admin_username = "azureuser"
-    admin_password = "Password1234!"
+    computer_name  = "${var.jumpvm_hostname}"
+    admin_username = "${var.admin_username}"
   }
 
   os_profile_linux_config {
     disable_password_authentication = true
 
     ssh_keys {
-      path     = "/home/azureuser/.ssh/authorized_keys"
+      path     = "/home/ubuntu/.ssh/authorized_keys"
       key_data = "${var.ssh_key_public}"
     }
   }
 
-  tags                          = "${var.tags}"
+  tags = "${var.tags}"
 }
--- terraform/azureControlPlaneHome/main.tf
+++ terraform/azureControlPlaneHome/main.tf
@@ -8,10 +8,28 @@ resource "azurerm_resource_group" "rg" {
 }
 
 # -----------------------------------------------------------------------------
+# remote state Saving
+# https://www.terraform.io/docs/backends/types/azurerm.html
+# -----------------------------------------------------------------------------
+# storage account RG: terraformstate-rg
+# storage account name: terraformstatesg
+# container name: tfstate
+# key: abconf.terraform.quickVnetState
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "terraformstate-rg"
+    storage_account_name = "terraformstatesg"
+    container_name       = "tfstate"
+    key                  = "abconf.terraform.azureControlPlaneHome"
+  }
+}
+
+# -----------------------------------------------------------------------------
 # vnet configuration
 # -----------------------------------------------------------------------------
 module "network" "azureControlPlaneNetwork" {
-  source              = "github.com/Azure/terraform-azurerm-network"
+  #source              = "github.com/Azure/terraform-azurerm-network"
+  source              = "Azure/network/azurerm"
   resource_group_name = "${azurerm_resource_group.rg.name}"
   location            = "${var.location}"
   address_space       = "${var.address_space}"
