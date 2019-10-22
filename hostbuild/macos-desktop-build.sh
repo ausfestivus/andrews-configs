6ca65a42499f9e8413edfd885bda6996621cd6a0
ausfestivus <abest@diaxion.com>
2019-10-22 19:58:47 +1100

    Update macos-desktop-build.sh

--- hostbuild/macos-desktop-build.sh
+++ hostbuild/macos-desktop-build.sh
@@ -934,7 +934,7 @@ set -o pipefail
 # Make sure we are signed into the app store
 notice "You must be signed into the App Store for this script to work..."
 notice "We will pause here while you go and do the sign in thing..."
-read -n 1 -s -r -p "Press any key to continue"
+read -s -r -p "Press enter to continue"
 
 # Run your script
 mainScript
