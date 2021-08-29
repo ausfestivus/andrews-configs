903759427935021b99b551cbb1a8acab6118c9ff
Andrew Best <festivus+git@gmail.com>
2021-08-29 21:02:58 +1000

    removes the logic to open the app store app and prompt for user password.

--- hostbuild/macos-desktop-build.sh
+++ hostbuild/macos-desktop-build.sh
@@ -144,26 +144,28 @@ function mainScript() {
         notice "$item --> $appName"
       fi
     }
-    if [[ $INSTALLCOMMAND =~ mas ]]; then # Log in to the Mac App Store if using mas
-      # TODO Must work out a way to open app store and prompt for sign in.
-      ##
-      # mas signin with MFA doesnt work.
-      # ISSUE https://github.com/ausfestivus/andrews-configs/issues/1
-      #mas signin $macStoreUsername "$macStorePass"
-      # WORKAROUND from https://github.com/mas-cli/mas/issues/164
-      # sign into the app store manually before running this.
-      # mas signout
-      # input "Please enter your Mac app store username: "
-      # read macStoreUsername
-      # input "Please enter your Mac app store password: "
-      # read -s macStorePass
-      # echo ""
-      ##
-      warning "You must be signed into the App Store for this script to work..."
-      warning "We will pause here while you go and do the sign in thing..."
-      open -a /System/Applications/App\ Store.app
-      read -n 1 -s -r -p "Press enter to continue"
-    fi
+    # FIXME the read here doesnt work for an unknown reason. This is disabled till I have time to work out why.
+    # if [[ $INSTALLCOMMAND =~ mas ]]; then # Log in to the Mac App Store if using mas
+    #   # TODO Must work out a way to open app store and prompt for sign in.
+    #   ##
+    #   # mas signin with MFA doesnt work.
+    #   # ISSUE https://github.com/ausfestivus/andrews-configs/issues/1
+    #   #mas signin $macStoreUsername "$macStorePass"
+    #   # WORKAROUND from https://github.com/mas-cli/mas/issues/164
+    #   # sign into the app store manually before running this.
+    #   # mas signout
+    #   # input "Please enter your Mac app store username: "
+    #   # read macStoreUsername
+    #   # input "Please enter your Mac app store password: "
+    #   # read -s macStorePass
+    #   # echo ""
+    #   ##
+    #   # warning "You must be signed into the App Store for this script to work..."
+    #   # warning "We will pause here while you go and do the sign in thing..."
+    #   # open -a /System/Applications/App\ Store.app
+    #   # read -n 1 -s -r -p "Press enter to continue"
+    #   true
+    # fi
 
     list=($(to_install "${RECIPES[*]}" "$(${LISTINSTALLED})"))
 
